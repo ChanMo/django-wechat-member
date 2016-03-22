e99932ea9756e7685b2835c0258c6527422a97b1
ChanMo <chen.orange@aliyun.com>
2016-03-22 10:01:32 +0800

    add mobile field

--- wechat_member/admin.py
+++ wechat_member/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Member
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('name', 'avatar_url', 'openid', 'created')
+    list_display = ('name', 'avatar_url', 'mobile', 'openid', 'created')
     list_filter = ('created',)
     list_per_page = 20
     search_fields = ['name']
--- wechat_member/models.py
+++ wechat_member/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Member(models.Model):
     name = models.CharField(max_length=200, verbose_name='姓名')
     avatar = models.CharField(max_length=200, verbose_name='头像')
+    mobile = models.CharField(max_length=20, blank=True, null=True,  verbose_name='手机号码')
     openid = models.CharField(max_length=200, verbose_name='微信ID')
     city = models.CharField(max_length=200, verbose_name='城市')
     created = models.DateTimeField(auto_now_add=True, verbose_name='注册时间')
@@ -21,5 +22,5 @@ class Member(models.Model):
         verbose_name = '会员'
         verbose_name_plural = '会员'
 
-    avatar_url.allow_tag = True
+    avatar_url.allow_tags = True
     avatar_url.verbose_name='头像'
