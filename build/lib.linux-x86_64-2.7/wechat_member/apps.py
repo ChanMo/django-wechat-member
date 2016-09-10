c1985a5315049b25800948ff4b65dc632f9ae363
ChanMo <chen.orange@aliyun.com>
2016-09-10 16:51:19 +0800

    remove build dirs

--- build/lib.linux-x86_64-2.7/wechat_member/apps.py
+++
@@ -1,6 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-from django.apps import AppConfig
-
-class WechatMemberConfig(AppConfig):
-    name = 'wechat_member'
-    verbose_name = _('wechat member')
--- build/lib.linux-x86_64-2.7/wechat_member/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^wx_auth/$', views.WxAuth.as_view(), name='auth'),
-]
