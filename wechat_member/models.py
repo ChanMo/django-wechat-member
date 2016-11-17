06eff3172c07c1245c45de3f63c21435b66ebaff
ChanMo <chen.orange@aliyun.com>
2016-11-17 13:26:43 +0800

    add python3

--- wechat_member/models.py
+++ wechat_member/models.py
@@ -1,7 +1,10 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.utils.html import format_html
 from django.db import models
 
+@python_2_unicode_compatible
 class Member(models.Model):
     name = models.CharField(_('name'), max_length=200)
     avatar = models.CharField(_('avatar'), max_length=200)
@@ -9,9 +12,6 @@ class Member(models.Model):
     city = models.CharField(_('city'), max_length=200)
     created = models.DateTimeField(_('created time'), auto_now_add=True)
     updated = models.DateTimeField(_('updated time'), auto_now=True)
-
-    def __unicode__(self):
-        return self.name
 
     def __str__(self):
         return self.name
